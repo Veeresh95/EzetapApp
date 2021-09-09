20bc9d1703f4a6541ec35b4d99d76d07e28216cb
Veeresh kumar b <veeruveeresh730@gmail.com>
2021-09-09 07:09:38 +0530

    done

--- app/src/main/java/com/example/ezetapapp/AppModule/MainActivity.java
+++ app/src/main/java/com/example/ezetapapp/AppModule/MainActivity.java
@@ -6,7 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ezetapapp.NetworkModule.ApiNetwork;
@@ -14,6 +20,8 @@ import com.example.ezetapapp.NetworkModule.RetrofitClient;
 import com.example.ezetapapp.PojoClasses.Response;
 import com.example.ezetapapp.PojoClasses.Uidata;
 import com.example.ezetapapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,8 +33,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextInputEditText name,mobile,city;
+    TextView header;
+    ImageView img;
     ProgressDialog pDialog;
     Context context;
+    Button submit;
     ArrayList<Uidata>dataArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +46,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         context=this;
         initViews();
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (name.getText().toString().trim().isEmpty()){
+                    Toast.makeText(context, "Please enter your name !!", Toast.LENGTH_SHORT).show();
+                    name.requestFocus();
+                    return;
+                }
+
+                if (mobile.getText().toString().trim().isEmpty()){
+                    Toast.makeText(context, "Please enter your mobile number !!", Toast.LENGTH_SHORT).show();
+                    mobile.requestFocus();
+                    return;
+                }
+
+                if (city.getText().toString().trim().isEmpty()){
+                    Toast.makeText(context, "Please enter your city !!", Toast.LENGTH_SHORT).show();
+                    city.requestFocus();
+                    return;
+                }
+
+                Intent i=new Intent(context,SecondActivity.class);
+                i.putExtra("name",name.getText().toString().trim());
+                i.putExtra("mobile",mobile.getText().toString().trim());
+                i.putExtra("city",city.getText().toString().trim());
+                startActivity(i);
+            }
+        });
     }
 
     private void initViews() {
+        img=findViewById(R.id.img);
+        header=findViewById(R.id.heading);
+        name=findViewById(R.id.full_name);
+        mobile=findViewById(R.id.mobile);
+        city=findViewById(R.id.city);
+        submit=findViewById(R.id.submit);
         loadJSON();
     }
 
@@ -50,8 +98,7 @@ public class MainActivity extends AppCompatActivity {
         pDialog.setCancelable(false);
         pDialog.show();
 
-        dataArrayList = new ArrayList<>();/*
-        Retrofit retrofit=new Retrofit.Builder().baseUrl("https://makeup-api.herokuapp.com/").addConverterFactory(GsonConverterFactory.create()).build();
+        /*Retrofit retrofit=new Retrofit.Builder().baseUrl("https://makeup-api.herokuapp.com/").addConverterFactory(GsonConverterFactory.create()).build();
         ApiNetwork api=retrofit.create(ApiNetwork.class);*/
 
         ApiNetwork api = RetrofitClient.getClient().create(ApiNetwork.class);
@@ -65,7 +112,21 @@ public class MainActivity extends AppCompatActivity {
                 assert response.body() != null;
                 try {
 
-                    dataArrayList=response.body().getUidata();
+                    Uri uri1 = Uri.parse(response.body().logoUrl);
+                    context = img.getContext();
+
+
+                    Picasso.with(context)
+                            .load(uri1)
+                            .placeholder(R.drawable.ic_launcher_background) //this is optional the image to display while the url image is downloading
+                            .error(R.drawable.ic_launcher_background)         //this is also optional if some error has occurred in downloading the image this image would be displayed
+                            .into(img);
+
+                    header.setText(response.body().getHeadingText());
+                    name.setHint(response.body().getUidata().get(1).getHint());
+                    mobile.setHint(response.body().getUidata().get(3).getHint());
+                    city.setHint(response.body().getUidata().get(5).getHint());
+
                 }
                 catch (NullPointerException exception){
                     Toast.makeText(context, exception.getMessage().toString(), Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/ezetapapp/AppModule/SecondActivity.java
+++ app/src/main/java/com/example/ezetapapp/AppModule/SecondActivity.java
@@ -2,15 +2,35 @@ package com.example.ezetapapp.AppModule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.ConditionVariable;
+import android.widget.TextView;
 
 import com.example.ezetapapp.R;
 
 public class SecondActivity extends AppCompatActivity {
 
+    Context context;
+    TextView name,mobile,city;
+    String naMe,moBile,ciTy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        context=this;
+
+        name=findViewById(R.id.name);
+        mobile=findViewById(R.id.mobile);
+        city=findViewById(R.id.city);
+
+        naMe=getIntent().getStringExtra("name");
+        moBile=getIntent().getStringExtra("mobile");
+        ciTy=getIntent().getStringExtra("city");
+
+        name.setText(naMe);
+        mobile.setText(moBile);
+        city.setText(ciTy);
+
     }
 }
